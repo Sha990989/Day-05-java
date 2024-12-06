bf57e4f15237308024fc9539a30416e27505dc14
Sha990989 <anavaneethasankar@gmail.com>
2024-12-06 21:54:42 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,20 @@
+/*1 2 3 4 5 
+  2 3 4 5
+  3 4 5 
+  4 5 
+  5*/
+  import java.util.*;
+public class pattern3
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(j+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
